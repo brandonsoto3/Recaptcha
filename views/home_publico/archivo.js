d46bafbc81afa5602b63cc5995d2043df99828f0
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2021-08-25 13:37:48 -0600

    Update archivo.js

--- views/home_publico/archivo.js
+++ views/home_publico/archivo.js
@@ -5,7 +5,7 @@ const constraints = {
   const video = document.querySelector("video");
   
   navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-    video.srcObject = stream;
+    //video.srcObject = stream;
     console.log('Camra funcional')
   });
   
