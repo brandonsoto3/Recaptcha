8cc8e3d963fa8a8dfd119487522ce02b990c2e27
brandonsoto3 <30421029+brandonsoto3@users.noreply.github.com>
2021-08-25 14:26:15 -0600

    Ayuda

--- views/home_publico/archivo.js
+++ views/home_publico/archivo.js
@@ -3,7 +3,7 @@ const constraints = {
     audio:true
   };
   
-  const video = document.querySelector("video");
+  //const video = document.querySelector("video");
   
   navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
     //video.srcObject = stream;
